73dddd9bdc4ed19d658fd805d2666d4c4f1b4b52
Sun <sunwenyen@gmail.com>
2020-08-20 21:46:04 +0800

    MotionLayoutSamplesDemo

---
+++ app/src/main/java/com/home/motionlayoutsamplesdemo/common/MotionToolBar.kt
@@ -0,0 +1,21 @@
+package com.home.motionlayoutsamplesdemo.common
+
+import android.content.Context
+import androidx.constraintlayout.motion.widget.MotionLayout
+import com.google.android.material.appbar.AppBarLayout
+import android.util.AttributeSet
+
+class MotionToolBar @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : MotionLayout(context, attrs, defStyleAttr), AppBarLayout.OnOffsetChangedListener {
+
+    override fun onOffsetChanged(appBarLayout: AppBarLayout?, verticalOffset: Int) {
+        val seekPosition = -verticalOffset / (appBarLayout?.totalScrollRange!!.toFloat() / 5 * 3)
+        progress = 1 - seekPosition;
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        (parent as? AppBarLayout)?.addOnOffsetChangedListener(this)
+    }
+}
---
+++ app/src/main/java/com/home/motionlayoutsamplesdemo/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.home.motionlayoutsamplesdemo.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.home.motionlayoutsamplesdemo.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
